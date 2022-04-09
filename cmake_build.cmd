7aab421f4e2e419f9d11dbec040e46b670024992
Shameer Kashif <sheikhshameerkashif@protonmail.com>
2022-04-09 14:37:27 +0500

    Updated Visual Studio version in cmake command line

--- cmake_build.cmd
+++ cmake_build.cmd
@@ -3,7 +3,7 @@ mkdir build
 
 cd build
 
-cmake -G "Visual Studio 17 2022" -DCMAKE_SYSTEM_VERSION=10.0.15063.0 ..
+cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_SYSTEM_VERSION=10.0.15063.0 ..
 cmake --build . --config Release
 cd ..
 
